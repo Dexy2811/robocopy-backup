5dc6f4f0676821909fae8098160f87a3f0a8dcae
Dexter <helpdextron@gmail.com>
2020-11-15 22:03:55 +0100

    uploaded script

---
+++ Backup.bat
@@ -0,0 +1,6 @@
+robocopy "networkshare path1" "networkshare path2" /b /s /sec
+:: 
+:: /b chooses backup mode.
+:: /s copies all subdirectorys accept for empty ones.
+:: /sec keeps all security settings.
+:: 
